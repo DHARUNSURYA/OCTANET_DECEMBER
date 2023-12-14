122b2c0d933421a7048a7b39f380bdb742a98c26
DHARUNSURYA <121680349+DHARUNSURYA@users.noreply.github.com>
2023-12-13 22:42:04 -1200

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,24 +1,33 @@
-document.querySelector('ul').addEventListener('click', function (event) {
-    if (event.target.tagName === 'LI') {
-        event.target.classList.toggle('gray');
-    } else if (event.target.tagName === 'SPAN') {
-        var parentLi = event.target.parentElement;
-        parentLi.style.transition = 'opacity 1s'; 
-        parentLi.style.opacity = '0'; 
-        setTimeout(function () {
-            parentLi.remove(); 
-        }, 1000);
-        event.stopPropagation();
-    }
-});
 
-document.querySelector('input').addEventListener('keypress', function (event) {
-    if (event.which === 13) {
-        var newTodo = this.value;
-        this.value = '';
-        var ul = document.querySelector('ul');
-        var li = document.createElement('li');
-        li.innerHTML = '<span>X</span> ' + newTodo;
-        ul.appendChild(li);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.container ul').addEventListener('click', function (event) {
+        if (event.target.tagName === 'LI' || event.target.tagName === 'SPAN') {
+            if (event.target.tagName === 'LI') {
+                event.target.classList.toggle('gray');
+            }
+            
+            if (event.target.tagName === 'SPAN') {
+                var parentLi = event.target.parentElement;
+                parentLi.style.transition = 'opacity 1s';
+                parentLi.style.opacity = '0';
+                setTimeout(function () {
+                    parentLi.remove();
+                }, 1000);
+                event.stopPropagation();
+            }
+        }
+    });
+
+    document.querySelector('.container input').addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            var newTodo = this.value.trim();
+            if (newTodo !== '') {
+                this.value = '';
+                var ul = document.querySelector('.container ul');
+                var li = document.createElement('li');
+                li.innerHTML = '<span>X</span> ' + newTodo;
+                ul.appendChild(li);
+            }
+        }
+    });
 });
